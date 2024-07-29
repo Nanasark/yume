7a5c0081094a3c4f615062cbed238024bb30fe6b
Nanasark <danielnyamekyesarkodie@gmail.com>
2024-07-29 13:04:31 +0000

    hover

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -33,19 +33,19 @@ export default function Hero() {
       </div>
       {/* Hero Right */}
       <div className="flex  w-1/2 ">
-        <div className="relative z-4  hover:pulse w-[280px] h-[450px] ">
+        <div className="relative z-4  pulse w-[280px] h-[450px] ">
           <Image
             src="/images/hp1.png"
-            className="relative z-20 hover:pulse w-[258px] h-[438px] swing-hp1"
+            className="relative z-20  w-[258px] h-[438px] swing-hp1"
             width={258}
             height={438}
             alt="face 1"
           />
         </div>
-        <div className="relative top-10 z-10 right-28">
+        <div className="relative  hover:animate-pulse top-10 z-10 right-28">
           <Image
             src="/images/hp2.png"
-            className="relative z-3 swing-hp2"
+            className="relative  z-3 swing-hp2"
             width={215}
             height={312}
             alt="face 1"
