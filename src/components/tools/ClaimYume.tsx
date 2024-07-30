fc33700b26d3763c7dc762c40c982bc67551bc75
Nanasark <danielnyamekyesarkodie@gmail.com>
2024-07-30 19:09:55 +0000

    transaction button for claim

--- src/components/tools/ClaimYume.tsx
+++ src/components/tools/ClaimYume.tsx
@@ -5,44 +5,63 @@ import { tokencontract } from "@/app/contract";
 import config from "@/Strings/config";
 import { useActiveAccount } from "thirdweb/react";
 import { toWei } from "thirdweb";
-import { ErrorHandler } from "../error/error";
+import { ErrorAlert, ErrorHandler } from "../error/error";
 import { Account } from "thirdweb/wallets";
 import { claimcontract } from "@/app/contract";
+import { TransactionButton } from "thirdweb/react";
 
 export default function ClaimYume() {
   const account = useActiveAccount();
   const address = account?.address || config.AuctionAddres;
 
-  const { mutate: sendTx } = useSendTransaction();
+  // const { mutate: sendTx } = useSendTransaction();
 
-  const ClaimFunction = async () => {
-    try {
-      const transaction = (await prepareContractCall({
-        contract: claimcontract,
-        method: "claim",
-        params: [address, toWei("50")],
-      })) as PreparedTransaction;
+  // const ClaimFunction = async () => {
+  //   try {
+  //     const transaction = (await prepareContractCall({
+  //       contract: claimcontract,
+  //       method: "claim",
+  //       params: [address, toWei("1000")],
+  //     })) as PreparedTransaction;
 
-      const { transactionHash } = await sendTransaction({
-        transaction,
-        account: account as Account,
-      });
+  //     const { transactionHash } = await sendTransaction({
+  //       transaction,
+  //       account: account as Account,
+  //     });
 
-      transactionHash ? alert("claimed") : alert("sorry claim another time");
-    } catch (error) {
-      ErrorHandler(error);
-      console.error(error);
-    }
-  };
+  //     transactionHash ? alert("claimed") : alert("sorry claim another time");
+  //   } catch (error) {
+  //     ErrorHandler(error);
+  //     console.error(error);
+  //   }
+  // };
 
   return (
     <div className="w-1/3">
-      <button
+      {/* <button
         onClick={ClaimFunction}
         className="w-200px ring-black bg-green-600 h-[60px] rounded-sm"
       >
-        claim ARYM
-      </button>
+        claim 1000 ARYM
+      </button> */}
+
+      <TransactionButton
+        transaction={() =>
+          prepareContractCall({
+            contract: claimcontract,
+            method: "claim",
+            params: [address, toWei("1000")],
+          })
+        }
+        onTransactionConfirmed={() => {
+          alert("claim sucessful");
+        }}
+        onError={ErrorAlert}
+        unstyled
+        className="w-200px ring-black bg-green-600 h-[60px] rounded-sm"
+      >
+        claim 1000 ARYM
+      </TransactionButton>
     </div>
   );
 }
